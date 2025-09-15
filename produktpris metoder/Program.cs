71e2e4725dad4f777766e2993bcd068e0d53ce3e
Semir Zahirovic <semir_guzonja@hotmail.se>
2025-09-15 13:14:58 +0200

    Add project files.

---
+++ produktpris metoder/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace produktpris_metoder
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
