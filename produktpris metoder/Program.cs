a4629d05277af3d06c9949bcbd5c698558a4ca63
Semir Zahirovic <semir_guzonja@hotmail.se>
2025-09-15 13:25:39 +0200

    nästan klar

--- produktpris metoder/Program.cs
+++ produktpris metoder/Program.cs
@@ -1,10 +1,28 @@
-﻿namespace produktpris_metoder
+﻿
+namespace produktpris_metoder
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            
+            Console.WriteLine("Product calculator");
+            Console.WriteLine("How many products do you wanna buy?");
+            var totalProducts = int.Parse(Console.ReadLine());
+            Console.WriteLine("How much does one product cost?");
+            var productPrice = int.Parse(Console.ReadLine());
+            Console.WriteLine("how much is the VAT in percent?");
+            var vat = double.Parse(Console.ReadLine());
+
+            var totalPrice = CalculatePrice(totalProducts, productPrice, vat);
+            Console.WriteLine($"The total price inc. VAT is:{totalPrice} SEK");
+        }
+
+        private static double CalculatePrice(
+            int totalProducts, 
+            int productPrice, 
+            double vat)
+        {
+            var totalPrice = (totalProducts)*(productPrice
         }
     }
 }
